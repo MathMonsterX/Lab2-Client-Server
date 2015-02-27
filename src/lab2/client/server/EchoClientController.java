c72b5e23cfa9fe32a7c07fd09b9beecbcf9c0540
MMX <trudosm@plu.edu>
2015-02-27 08:42:48 -0800

    Initializing files
    Added the files needed for the project.

---
+++ src/lab2/client/server/EchoClientController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2.client.server;
+
+/**
+ *
+ * @author Sheyla
+ */
+public class EchoClientController implements Controller{
+
+    @Override
+    public void update(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
