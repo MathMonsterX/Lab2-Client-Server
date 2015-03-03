1c775f9a6083a3dd67500709dc9706fd92a56708
MMX <trudosm@plu.edu>
2015-03-03 01:41:34 -0800

    MessagingModel Javadoc
    Created the javadocs for the messaging model. The messaging model is
    mainly in charge of storing server messages and any program errors in a
    log.

--- src/lab2/client/server/MessagingModel.java
+++ src/lab2/client/server/MessagingModel.java
@@ -23,10 +23,20 @@ public class MessagingModel {
     
     EchoClientController view;
     
+    /**
+     * Sets the controller of the messagingModel.
+     * @param newController     The controller for the model
+     */
     public void addController( EchoClientController newController ){ view = newController; }
-    public void updateValue( String value ){}
-    
-    public void logMessage( String txt )
+
+    /**
+     * Creates a log file where server messages are kept track of. 
+     * If the file ("log.txt") does not exist, one is created.
+     * Also informs the controller of errors in logging messages.
+     * Errors are also saved within the log.
+     * @param msg   The message to be logged
+     */
+    public void logMessage( String msg )
     {
         BufferedWriter bw = null;
         File f = null;
@@ -36,7 +46,7 @@ public class MessagingModel {
             if(!f.exists()) f.createNewFile();
             bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f, true)));
             
-            bw.append( txt + "\n");
+            bw.append(msg + "\n");
         } catch (IOException ex) {
             // report
             view.updateErrorText(ex.getMessage());
